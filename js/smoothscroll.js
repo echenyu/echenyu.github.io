593997facb0ff01bdfba9211d550d4ff0e340cf2
Eric Yu <echenyu@umich.edu>
2014-12-27 21:32:36 -0500

    first commit

---
+++ js/smoothscroll.js
@@ -0,0 +1,16 @@
+<script>
+
+  lp.jQuery(function($) {
+  
+    // The speed of the scroll in milliseconds
+    var speed = 1000;
+  
+    // Find links that are #anchors and scroll to them
+    $('a[href^=#]').not('.lp-pom-form .lp-pom-button').unbind('click.smoothScroll').bind('click.smoothScroll', function(event) {
+      event.preventDefault();
+      $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, speed);
+    });
+  
+  });
+
+</script>
