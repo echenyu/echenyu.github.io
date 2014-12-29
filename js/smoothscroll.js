55e07a2b6c8baccc223a83dd7222be2ae1060ae6
Eric Yu <echenyu@umich.edu>
2014-12-28 23:27:35 -0500

    Added in smooth scrolling for in page links; added skills circles and changed some of the text

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,16 +1,14 @@
-<script>
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
-  lp.jQuery(function($) {
-  
-    // The speed of the scroll in milliseconds
-    var speed = 1000;
-  
-    // Find links that are #anchors and scroll to them
-    $('a[href^=#]').not('.lp-pom-form .lp-pom-button').unbind('click.smoothScroll').bind('click.smoothScroll', function(event) {
-      event.preventDefault();
-      $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, speed);
-    });
-  
-  });
+	    var target = this.hash;
+	    var $target = $(target);
 
-</script>
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
